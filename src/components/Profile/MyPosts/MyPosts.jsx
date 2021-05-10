7f86f4cfa577868c3f65399ddd1d7ab96baed3c0
LuckySava <vivatukraine@yahoo.com>
2021-05-10 21:22:01 +0300

    ref example

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,25 @@
+import React from 'react';
 import s from './MyPosts.module.scss';
 import Post from './Post/Posts';
 
 const MyPosts = (props) => {
 
     let postsElements = props.posts.map(item => <Post message={item.message} likeCounter={item.likesCounter} />);
+
+    let addPostsText = React.createRef();
+
+    let addPost = () => {
+        let text = addPostsText.current.value;
+        console.log(text);
+    }
+
     return (
         <div>
+            <h3>My Posts</h3>
             <div className={s.new_post}>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea ref={addPostsText} name="" id="" cols="30" rows="10"></textarea>
                 <p>
-                    <button>Add Button</button>
+                    <button onClick={addPost}>Add Button</button>
                 </p>
             </div>
 
